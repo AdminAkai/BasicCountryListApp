3143a4e4348190cb0f2eeb5c5819dd3ddf12e96d
JCaptainnnn <joshietrinidad@gmail.com>
2021-03-10 19:03:46 -0500

    Basic country list app with dummy data

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StyleSheet, Text, Pressable, Alert } from 'react-native';
+import axios from 'axios'
 
 export default function App() {
+
+  const [countriesData, setCountriesData] = useState([])
+
+  const fetchCountriesData = async () => {
+    try {
+      const res = await axios.get('https://restcountries.eu/rest/v2/region/africa?fields=name;capital')
+      setCountriesData([...res.data])
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCountriesData()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={countriesData}
+      contentContainerStyle={styles.container}
+      keyExtractor={item => item.name}
+      renderItem={
+        ({item}) => 
+          <Text
+            onPress={() => {Alert.alert(`The Capital of ${item.name} is ${item.capital}`)}} 
+            style={styles.text} 
+            key={item.name}
+          >
+            {item.name}
+          </Text>
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: '#483D8B',
   },
+  text: {
+    fontSize: 18,
+    margin: 5,
+    color: '#fff'
+  }
 });
